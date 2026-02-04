7c302d46b6208ce5c897e1cb7501c13913d4621d
debelak4 <danielle@harbenmarketing.com>
2026-02-04 12:44:44 -0600

    Update nightwatch_tools.js

--- nightwatch_tools.js
+++ nightwatch_tools.js
@@ -1,51 +1,30 @@
-javascript:(function() {
-  var editButtons = document.querySelectorAll('.nw-table-column__col-actions a');
-  var index = 0;
-  var windows = [];
+//https://cmc.harbenmarketing.com/moz/nightwatch_tools.js
 
-  function processReport() {
-    if (index < editButtons.length) {
-      var button = editButtons[index];
-      var reportUrl = button.getAttribute('href');
-      var newWindow = window.open(reportUrl, '_blank');
-      windows.push(newWindow);
 
-      newWindow.addEventListener('load', function() {
-        // Wait for the Download button to appear
-        var downloadButtonInterval = setInterval(function() {
-          var downloadButton = findDownloadButton(newWindow.document);
-          if (downloadButton) {
-            clearInterval(downloadButtonInterval);
-            downloadButton.click();
-            index++;
-            processReport();
-          }
-        }, 100); // Check for the download button every 100 milliseconds
+alert("Nightwatch tools loaded, starting SERP Downloads - updated 6-8-2023");
+
+
+downloadKeywordSERPReports();
+
+function openEditIconsAndDownloadReports() {
+  const editIcons = document.querySelectorAll('.nw-icon');
+
+  editIcons.forEach((editIcon) => {
+    editIcon.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevents the default click behavior
+
+      const editLink = editIcon.parentNode.href;
+      const newWindow = window.open(editLink, '_blank');
+      newWindow.focus();
+
+      newWindow.addEventListener('load', () => {
+        const downloadButton = newWindow.document.querySelector('.ember-view.nw-btn.nw-btn--primary');
+        if (downloadButton) {
+          downloadButton.click();
+        }
       });
-    } else {
-      // All reports have been processed
-      setTimeout(function() {
-        closeAllWindows();
-      }, 15000); // Delay of 15 seconds before closing the windows
-    }
-  }
-
-  function findDownloadButton(document) {
-    var buttons = document.querySelectorAll('button');
-    for (var i = 0; i < buttons.length; i++) {
-      var button = buttons[i];
-      if (button.textContent.trim() === 'Download Report') {
-        return button;
-      }
-    }
-    return null;
-  }
-
-  function closeAllWindows() {
-    windows.forEach(function(window) {
-      window.close();
     });
-  }
+  });
+}
 
-  processReport();
-})();
+openEditIconsAndDownloadReports();
